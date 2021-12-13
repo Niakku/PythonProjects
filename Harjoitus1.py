9d420e816423636dc971e85a303b1cef7f4da0c5
Niakku <niakku.nieminen@gmail.com>
2021-12-13 18:14:49 +0200

    Harjoitus

---
+++ Harjoitus1.py
@@ -0,0 +1,17 @@
+first_name1 = "Juuso"
+
+
+first_name2 = "Elsa"
+
+
+tervehdys = "Kuulin, että pidät hevosista. Oletkos tiennyt tätä?"
+viesti = """Kesyhevonen (Equus caballus caballus) on yksi hevosen alalajeista.
+        Se on suurikokoinen kavioeläinten lahkoon ja hevoseläinten heimoon kuuluva nisäkäs. 
+        Ihminen kesytti hevosen arviolta jo noin 5500 vuotta sitten. Hevosta on käytetty 
+        muun muassa maidontuottajana, ratsuna sekä veto- ja kantojuhtana maatalous- ja metsätöissä. 
+        Traktorit ja autot ovat 1900-luvulla pitkälti korvanneet hevosen aiemmista käyttötarkoituksistaan. 
+        Nykyään hevosta pidetäänkin enemmän harrastus- kuin hyötyeläimenä."""
+
+fullName1 = f"{first_name1}"
+fullName2 = f"{first_name2}"
+print(f"Moi {fullName2}!\n\n{tervehdys}\n\n{viesti}\n\nTerveisin {fullName1}")
